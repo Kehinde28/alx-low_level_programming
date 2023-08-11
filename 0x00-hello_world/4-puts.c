828617899676c0fb59bf59ac29323a9a23a2ccf0
Kehinde28 <orebanwoirewolede@gmail.com>
2023-08-11 06:09:10 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
-/**
- *main - entry point
- *return: always 0 (sucess)
- */
+#include<stdio.h>
+ /**
+  * main - Entry point
+  * Return: Always 0 (Success)
+  */
 int main(void)
 {
-	puts("\"programming is like building a multilinqual puzzle");
-	retrun (0);
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
 }
