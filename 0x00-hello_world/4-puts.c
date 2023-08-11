34d68ad3575fcea08e7163de391d1698aa59ba19
Kehinde28 <orebanwoirewolede@gmail.com>
2023-08-10 17:27:59 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,7 +6,7 @@
  */c
 int main(void)
 {
-      puts(\"Programming is like building a multilingual puzzle");
+      puts("Programming is like building a multilingual puzzle");
 
        retun (0);
 }
