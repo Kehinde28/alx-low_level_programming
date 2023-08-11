df46363e838d319f4e44a54701686ccb3982c3d7
Kehinde28 <orebanwoirewolede@gmail.com>
2023-08-11 11:50:18 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,14 @@
-#include<stdio.h>									
+#include <stdio.h>
 #include <stdlib.h>
 /**
-  * main - Entry program that prints a proper grammer
-  * Return: 0
-  */												
-int main()
+ * main - Entry program that prints a proper grammer
+ * Return: 0
+ */
+
+int main(void)
 {
+	int n;
+
 	puts("\"programming is like building a multilingual puzzle");
 	return (0);
 }
