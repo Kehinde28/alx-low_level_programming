8a8dd8fbae087a80693d74060bcaf496e8c70450
Kehinde28 <orebanwoirewolede@gmail.com>
2023-08-11 02:55:16 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main(void)
 {
-      puts("\"Programming is like building a multilingual puzzle");
+   puts("\"Programming is like building a multilingual puzzle");
 
-       retun 0;
+      retun 0;
 }
