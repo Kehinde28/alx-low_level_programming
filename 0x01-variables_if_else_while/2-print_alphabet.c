4d56c6a738322a8f63cabf2685fd34b42baf7718
Kehinde28 <orebanwoirewolede@gmail.com>
2023-08-13 15:25:52 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - program that prints the alphabet in lowercase
+ * Return: 0
+*/
+
+int main()
+{
+	putchat[] = ("a");
+	printf("hello %s is good");
+	return (0);
+
+}
