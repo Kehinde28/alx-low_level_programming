24a959c99a02c37148beba8d7538de1fbbbe6c30
Kehinde28 <orebanwoirewolede@gmail.com>
2023-08-13 16:50:48 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,18 +8,18 @@
 
 int main(void)
 {
-    int ab;
+	char ab;
 
-    for (ab = 'a'; ab <= 'z'; ab++)
-    {
-        putchar(ab);
-    }
+	for (ab = 'a'; ab <= 'z'; ab++)
+	{
+		putchar(ab);
+	}
 
-    for (ab = 'A'; ab <= 'Z'; ab++)
-    {
-        putchar(ab);
-    }
+	for (ab = 'A'; ab <= 'Z'; ab++)
+	{
+		putchar(ab);
+	}
 
-    putchar('\n');
-    return (0);
+	putchar('\n');
+	return (0);
 }
