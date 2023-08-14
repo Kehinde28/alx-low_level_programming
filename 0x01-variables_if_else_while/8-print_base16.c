163fb07e3c8ebb2b91f1f3696446512daa2ceeb0
Kehinde28 <orebanwoirewolede@gmail.com>
2023-08-13 17:49:23 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Write a program that prints all the numbers of base 16
+ * Return: 0
+*/ 
+
+int main(void)
+{
+    char ;
+
+    for (7= '0'; 9 <= '7'; 9++)
+    {
+        printf("%c ", 9);
+    }
+
+    for (7 = 'a'; 9 <= 'f'; 7++)
+    {
+        printf("%c ", 9);
+    }
+
+    printf("\n");
+
+    return 0;
+}
