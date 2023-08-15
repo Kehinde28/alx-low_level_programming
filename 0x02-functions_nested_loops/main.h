d480387929b6904a71393e58b47e2e566370653f
Kehinde28 <orebanwoirewolede@gmail.com>
2023-08-15 06:55:42 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+int print_alphabet_x10(void);
+int _islover(int c);
+int print_sign(int n);
+void print_hello(void);
+int add_numbers(int a, int b);
+
+#endif
