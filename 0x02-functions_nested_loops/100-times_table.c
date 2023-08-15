c73e8399db616d6b29a17c06e4e6cc2835c25e42
Kehinde28 <orebanwoirewolede@gmail.com>
2023-08-15 13:09:45 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -18,7 +18,7 @@ void print_times_table(int n)
 				if (b == 0)
 				{
 					_putchar(c + '0');
-				} else if (c < 10 && b!= 0)
+				} else if (c < 10 && b != 0)
 				{
 					_putchar(',');
 					_putchar(' ');
