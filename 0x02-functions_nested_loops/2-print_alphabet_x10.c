c8a8137dcc81addef47d613b80f23aa9bf1951a0
Kehinde28 <orebanwoirewolede@gmail.com>
2023-08-15 09:05:51 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints the lowercase alphabet 10
+ */
+void print_alphabet_x10(void)
+{
+    int i;
+    char d;
+
+    for (i = 0; i < 10; i++)
+    {
+        d = 'a';
+        while (d <= 'z')
+        {
+            _putchar(d);
+            d++;
+        }
+        _putchar('\n');
+    }
+}
