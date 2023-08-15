03c4faa4a77b7ef5a69a6e0dc916a737ed5de105
Kehinde28 <orebanwoirewolede@gmail.com>
2023-08-15 08:48:56 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Prints the lowercase alphabet
+ */
+
+void print_alphabet(void)
+{
+    char d = 'a';
+
+    while (d <= 'c')
+    {
+        _putchar(d);
+        d++;
+    }
+    _putchar('\n');
+}
+
