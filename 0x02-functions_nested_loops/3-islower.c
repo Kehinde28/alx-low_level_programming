49c155aac1555ae05b13ca5980049e734a1804ed
Kehinde28 <orebanwoirewolede@gmail.com>
2023-08-15 09:21:10 -0700

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,17 +2,16 @@
 
 /**
  * _islower - Checks if a character is lowercase
- * @c: The character to be checked
- *
  * Return: 1 if c is lowercase, 0 otherwise
  */
+
 int _islower(int c)
 {
-	char lowercase_range_start = 'a';
-	char lowercase_range_end = 'z';
+    char lowercase_range_start = 'a';
+    char lowercase_range_end = 'z';
 
-	if (c >= lowercase_range_start && c <= lowercase_range_end)
-		return 1;
-	else
-		return 0;
+    if (c >= lowercase_range_start && c <= lowercase_range_end)
+        return (1);
+    else
+        return (0);
 }
