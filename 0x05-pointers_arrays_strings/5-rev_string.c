f44d915180217f7f42c1bb91adeba6e830088169
Kehinde28 <orebanwoirewolede@gmail.com>
2023-08-21 07:21:07 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,17 +8,17 @@
  */
 void rev_string(char *s)
 {
-    char rev;
-    int meter = 0;
-    int i;
+		char rev;
+		int meter = 0;
+		int i;
 
-    while (s[meter] != '\0')
-        meter++;
+		while (s[meter] != '\0')
+		meter++;
 
-    for (i = 0; i < meter / 2; i++)
-    {
-        rev = s[i];
-        s[i] = s[meter - 1 - i];
-        s[meter - 1 - i] = rev;
-    }
+		for (i = 0; i < meter / 2; i++)
+		{
+		rev = s[i];
+		s[i] = s[meter - 1 - i];
+		s[meter - 1 - i] = rev;
+		}
 }
