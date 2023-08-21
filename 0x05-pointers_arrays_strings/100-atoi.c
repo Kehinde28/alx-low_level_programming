fd84f08e0dd6602e8c26759c037f7d676e741e43
Kehinde28 <orebanwoirewolede@gmail.com>
2023-08-21 08:48:56 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,46 +1,45 @@
 #include "main.h"
 
 /**
- * _atoi - converts a string to an integer
- * @s: string to be converted
- * Return: converted string
+ * _atoi - Converts a string to an integer
+ * @s: The string to be converted
+ *
+ * Return: The converted integer
  */
-
 int _atoi(char *s)
 {
-	int a, b, n, ball, c, roll;
+	int sign = 1;
+	int result = 0;
+	int i = 0;
 
-	a = 0;
-	b = 0;
-	n = 0;
-	ball = 0;
-	c = 0;
-	roll = 0;
-
-	while (s[ball] != '\0')
-		ball++;
-
-	while (a < ball && c == 0)
+	while (s[i] == ' ' || s[i] == '\t')
 	{
-		if (s[a] == '-')
-			++b;
-
-		if (s[a] >= '0' && s[a] <= '9')
-		{
-			roll = s[a] - '0';
-			if (d % 2)
-				roll = -roll;
-			n = n * 10 + roll;
-			c = 1;
-			if (s[a + 1] < '0' || s[a + 1] > '9')
-				break;
-			c = 0;
-		}
-		a++;
+		i++;
 	}
 
-	if (c == 0)
-		return (0);
+	if (s[i] == '-')
+	{
+		sign = -1;
+		i++;
+	}
+	else if (s[i] == '+')
+	{
+		i++;
+	}
 
-	return (n);
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		if (result > INT_MAX / 10 || (result == INT_MAX / 10 && s[i] - '0' > 7))
+		{
+			if (sign == 1)
+				return (INT_MAX);
+			else
+				return (INT_MIN);
+		}
+
+		result = result * 10 + (s[i] - '0');
+		i++;
+	}
+
+	return (sign * result);
 }
