a18384775b006ac486435f1bcd925a942d8b09f5
Kehinde28 <orebanwoirewolede@gmail.com>
2023-09-02 13:34:04 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -4,40 +4,40 @@
 /**
  * _atoi - converts a string to an integer
  * @s: string to be converted
+ *
  * Return: the int converted from the string
  */
-
 int _atoi(char *s)
 {
-	int a, d, n, len, f, digit;
+	int i, d, n, len, f, digit;
 
-	a = 0;
+	i = 0;
 	d = 0;
 	n = 0;
 	len = 0;
 	f = 0;
 	digit = 0;
 
-	while (s[sen] != '\0')
-		sen++;
+	while (s[len] != '\0')
+		len++;
 
-	while (a < sen && f == 0)
+	while (i < len && f == 0)
 	{
-		if (s[a] == '-')
+		if (s[i] == '-')
 			++d;
 
-		if (s[a] >= '0' && s[a] <= '9')
+		if (s[i] >= '0' && s[i] <= '9')
 		{
-			digit = s[a] - '0';
+			digit = s[i] - '0';
 			if (d % 2)
 				digit = -digit;
 			n = n * 10 + digit;
 			f = 1;
-			if (s[a + 1] < '0' || s[a + 1] > '9')
+			if (s[i + 1] < '0' || s[i + 1] > '9')
 				break;
 			f = 0;
 		}
-		a++;
+		i++;
 	}
 
 	if (f == 0)
@@ -50,12 +50,12 @@ int _atoi(char *s)
  * main - multiplies two numbers
  * @argc: number of arguments
  * @argv: array of arguments
- * Return: 0 or 1
+ *
+ * Return: 0 (Success), 1 (Error)
  */
-
 int main(int argc, char *argv[])
-{`
-	int result, sum1, sum2;
+{
+	int result, num1, num2;
 
 	if (argc < 3 || argc > 3)
 	{
@@ -63,12 +63,11 @@ int main(int argc, char *argv[])
 		return (1);
 	}
 
-	sum1 = _atoi(argv[1]);
-	sum2 = _atoi(argv[2]);
-	result = sum1 * sum2;
+	num1 = _atoi(argv[1]);
+	num2 = _atoi(argv[2]);
+	result = num1 * num2;
 
 	printf("%d\n", result);
 
 	return (0);
 }
-
