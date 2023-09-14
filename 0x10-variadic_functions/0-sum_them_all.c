35d5667aaadf42e87a442c2e6ff796616d313214
Kehinde28 <orebanwoirewolede@gmail.com>
2023-09-14 11:38:06 -0700

    0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -4,7 +4,7 @@
 /**
  * sum_them_all - Returns the sum of all its paramters
  * @n: The number of paramters passed to the function
- * @...: A variable number of paramters to calculate 
+ * @...: A variable number of paramters to calculate
  * Return: If n == 0 - 0
  */
 
