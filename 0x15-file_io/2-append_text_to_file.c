af52d88e69b37fd445a7cbf3c8150f115f4ffc77
Kehinde28 <orebanwoirewolede@gmail.com>
2023-10-03 13:31:52 -0700

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -10,7 +10,7 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int a, i, sen = 0;
+	int o, i, sen = 0;
 
 	if (filename == NULL)
 		return (-1);
@@ -21,13 +21,13 @@ int append_text_to_file(const char *filename, char *text_content)
 			sen++;
 	}
 
-	a = open(filename, O_WRONLY | O_APPEND);
-	i = write(a, text_content, sen);
+	o = open(filename, O_WRONLY | O_APPEND);
+	i = write(o, text_content, len);
 
-	if (a == -1 || w == -1)
+	if (o == -1 || w == -1)
 		return (-1);
 
-	close(a);
+	close(o);
 
 	return (1);
 }
